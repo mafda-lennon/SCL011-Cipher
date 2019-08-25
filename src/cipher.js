41242a6d894c8fb97aafa5be3ee693a794af5a89
mafda-lennon <mf.lennon@gmail.com>
2019-08-25 13:39:10 -0300

    añadido btn copiar/pegar + algo de html + algo de css

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,5 @@
 window.cipher = {
-  encode: (msje, offset) => { let ascii, alfEncode;
+  encode: (msje, offset) => { let ascii, alfEncode, ot = "";
     for(let i=0; i < msje.length; i++){
       ascii = msje[i].charCodeAt()
       if(ascii===32){
@@ -7,13 +7,13 @@ window.cipher = {
       }else{
         alfEncode = (parseInt(ascii) - 65 + offset) % 26 + 65;
         alfEncode = String.fromCharCode(alfEncode);
-        console.log(alfEncode);
       }
+      ot = ot + alfEncode;
     }
-      return alfEncode;
+    return ot;
   },
 
-  decode: (msje, offset) => { let ascii, alfEncode;
+  decode: (msje, offset) => { let ascii, alfEncode, ot = "";
     for(let i=0; i < msje.length; i++){
       ascii = msje[i].charCodeAt()
       if(ascii===32){
@@ -22,7 +22,8 @@ window.cipher = {
         alfEncode = (parseInt(ascii) - 90 - offset) % 26 + 90;
         alfEncode = String.fromCharCode(alfEncode);
       }
+      ot = ot + alfEncode;
     }
-      return alfEncode;
+    return ot;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
-let msje, offset;
+let user, pw, userData="fer", pwData="1234", msje, offset, email, copyBtn, otCopy, cleanBtn;
 
+//Funciones para mostrar y esconder elementos
+function hideContent(element) {
+	document.getElementById(element).style.display = 'none';
+}
+
+function showContent(element) {
+	document.getElementById(element).style.display = 'block';
+}
+
+//Llama a la función de hideContent para el siguiente paso en el Login
+hideContent("empty");
+hideContent("wrong");
+
+//Login
+document.getElementById("loginBtn").addEventListener("click", () => {
+    user = document.getElementById("user").value;
+    pw = document.getElementById("password").value;
+    if(user==userData && pw==pwData) { 
+        hideContent("login");
+        showContent("content");
+    }else if(user==="" || pw===""){showContent("empty")
+    }else if(user!=="fer" || pw!=="1234"){showContent("wrong")
+    }
+});
+
+//Botón offset
 document.getElementById("btnOffset").addEventListener("click", () => {
     offset = document.getElementById("offset").value;
     offset = parseInt(offset);
 });
 
+//Cifrar
 document.getElementById("encode").addEventListener("click", () => {
     msje = document.getElementById("inputText").value.toUpperCase();
     document.getElementById("outputText").innerHTML = window.cipher.encode(msje, offset);
 });
 
+//Descifrar
 document.getElementById("decode").addEventListener("click", () => {
     msje = document.getElementById("inputText").value;
     msje = msje.toUpperCase();
     document.getElementById("outputText").innerHTML = window.cipher.decode(msje, offset);
 });
+
+//Botón Copiar
+copyBtn = document.getElementById("copy").addEventListener("click", copy, false);
+function copy() {
+  otCopy = document.getElementById("outputText");
+  let inputFalso = document.createElement("input");
+  inputFalso.setAttribute("value", outputText.innerHTML);
+  document.body.appendChild(inputFalso);
+  inputFalso.select();
+  document.execCommand("copy");
+  document.body.removeChild(inputFalso);
+}
+
+//Botón Limpiar
+cleanBtn = document.getElementById("clean").addEventListener("click", () => {
+    cleanBtn = document.getElementById("inputText").innerHTML = "";
+    cleanBtn = document.getElementById("outputText").innerHTML = "";
+})
